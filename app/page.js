479c268eb5cd2009595d9599c9c0bf8c1f186a54
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-03 04:04:35 +0530

    gives link to the dashboard

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Collect customer feedback </h1>
       <div> Create a feedback board in minutes , prioritize features , and build products your customers will love</div>
+      <Link href= "/dashboard">Dashboard</Link>
     </main>
   );
 }
