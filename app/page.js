722a781f5a0f7dd26b0023117f7e70af833accbf
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-03 03:50:05 +0530

    subheading using the div tag

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Collect customer feedback </h1>
+      <div> Create a feedback board in minutes , prioritize features , and build products your customers will love</div>
     </main>
-  )
+  );
 }
