5e59c1a73dd3dc4c9f248e109e27ad92a7dd12e5
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-06 05:29:10 +0530

    Auth.js and mongodb connection

--- app/api/auth/route.js
+++
@@ -1,5 +0,0 @@
-export function GET() {
-    return Response.json({message: "You just hit an API 😎"});
-    }
-
-
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -10,6 +10,14 @@ if (props.isLoggedIn){
     return <Link href="/dashboard" className="btn btn-primary">Welcome back {props.name}</Link>
 } else {
     return <button>Login</button>;
+
+// 1. Create a /login page
+
+// 2. Create a email/password form
+
+// 3. Make a POST request to /api/login
+
+
 }
 };
 
