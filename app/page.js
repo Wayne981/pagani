03d12b1c96007f329e35b8fe0bbf7ebdf5d3f5ff
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-06 05:02:05 +0530

    API endpoint

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { Example } from "@/components/Example";
 import FAQListItem from "@/components/FAQListItem";
 import Image from "next/image";
 import productDemo from "@/app/productDemo.jpeg";
-import clientPromise from "@/libs/mongo";
+// import clientPromise from "@/libs/mongo";
 
 export default function Home() {
 const isLoggedIn = true; 
