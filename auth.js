2950ebdb2e4423933e408f54c26d457353676d97
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-07 17:41:13 +0530

    check 1

--- auth.js
+++ auth.js
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth";
-import { MongoDBAdapter} from "@auth/mongodb-adapter";
-import clientPromise from "./libs/mongo";
-import Resend from 
+// import NextAuth from "next-auth";
+// import { MongoDBAdapter} from "@auth/mongodb-adapter";
+// import clientPromise from "./libs/mongo";
+// import Resend from 
 
 
-const config = {
-    providers: [],
-    adapter: MongoDBAdapter(clientPromise),
-    // this mongodb adapter will have access to our database - now aut.js has access to ur mongodb database
+// const config = {
+//     providers: [],
+//     adapter: MongoDBAdapter(clientPromise),
+//     // this mongodb adapter will have access to our database - now aut.js has access to ur mongodb database
 
-};
+// };
 
-// const auth = NextAuth(config)
-// giving config file to next auth
-// as we are going to use it everywhere
-export const {handlers , signIn , signOut , auth} = NextAuth(config)
+// // const auth = NextAuth(config)
+// // giving config file to next auth
+// // as we are going to use it everywhere
+// export const {handlers , signIn , signOut , auth} = NextAuth(config)
