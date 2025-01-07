1a59b9eca556d06beee702eda08b2c1d48dee527
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-08 04:35:08 +0530

    Resend and Google authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
-// import { handlers } from "@/auth";
+import { handlers } from "@/auth";
 
-// const { GET , POST} = handlers
+export const GET = handlers.GET;
+export const POST = handlers.POST;
--- auth.js
+++ auth.js
@@ -1,17 +1,32 @@
-// import NextAuth from "next-auth";
-// import { MongoDBAdapter} from "@auth/mongodb-adapter";
-// import clientPromise from "./libs/mongo";
-// import Resend from 
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "./libs/mongo";
+import Resend from "next-auth/providers/resend";
+
+import Google from "next-auth/providers/google";
 
 
-// const config = {
-//     providers: [],
-//     adapter: MongoDBAdapter(clientPromise),
-//     // this mongodb adapter will have access to our database - now aut.js has access to ur mongodb database
 
-// };
+const config = {
+    providers: [
+        Resend({
+            apiKey : process.env.RESEND_KEY,
+            from: " noreply@resend.navaneethcaterers.com"
+         
+        }),
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
 
-// // const auth = NextAuth(config)
-// // giving config file to next auth
-// // as we are going to use it everywhere
-// export const {handlers , signIn , signOut , auth} = NextAuth(config)
+        }),
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+    // this mongodb adapter will have access to our database - now aut.js has access to ur mongodb database
+
+};
+
+// const auth = NextAuth(config)
+// giving config file to next auth
+// as we are going to use it everywhere
+export const {handlers , signIn , signOut , auth} = NextAuth(config);
+
