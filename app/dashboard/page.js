d4f7d7dc143bcbd7427ad3daa48b0d7611d26a7e
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-09 03:14:19 +0530

    Logout

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,16 @@
+import ButtonLogout from "@/components/ButtonLogout"
+
 export default function Dashboard() {
  return (
     <main>
         <h1>private dashboard</h1>
+
+        <ButtonLogout/>
     </main>
+
+   
  )
 }
+
+
+
