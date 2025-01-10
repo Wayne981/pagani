a909170911b9420b36a0f9beb11bf283ec434d94
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-01-10 14:54:21 +0530

    FormNewBoard component

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,16 +1,20 @@
-import ButtonLogout from "@/components/ButtonLogout"
+import ButtonLogout from "@/components/ButtonLogout";
+import FormNewBoard from "@/components/FormNewBoard";
 
 export default function Dashboard() {
- return (
-    <main>
-        <h1>private dashboard</h1>
+    return (
+        <main className="bg-base-200 min-h-screen">
+            {/* Header */}
+            <section className="bg-base-100">
+             <div className="max-w-5xl mx-auto px-5 py-3 flex justify-end">
+                {/* <h1>private dashboard</h1> */}
+                <ButtonLogout />
+                </div>
+            </section>
 
-        <ButtonLogout/>
-    </main>
-
-   
- )
+            <section className="max-w-5xl mx-auto px-5 py-12">
+                <FormNewBoard />
+            </section>
+        </main>
+    );
 }
-
-
-
---
+++ components/FormNewBoard.js
@@ -0,0 +1,21 @@
+const FormNewBoard = () => {
+    return (
+        // 1. TITLE
+        // 2. FORM
+        // 3. BUTTON
+        <div className="bg-base-100 p-8 rounded-3xl space-y-8">
+            <p className="font-bold text-lg">Create a new feedback board</p>
+            <label className="form-control w-full">
+                <div className="label-text">Board name</div>
+                <input 
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                />
+            </label>
+            <button className="btn btn-primary w-full">Create board</button>
+        </div>
+    );
+};
+
+export default FormNewBoard;
