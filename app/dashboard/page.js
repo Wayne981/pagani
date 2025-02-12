da87335b3f038c40cc567427295bc6adad231616
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-02-13 01:08:43 +0530

    User boards list

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,27 @@
 import ButtonLogout from "@/components/ButtonLogout";
 import FormNewBoard from "@/components/FormNewBoard";
+import { auth } from "@/auth"; // to get the user session
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+import "@/models/Board"; // This will ensure Board model is registered
 
-export default function Dashboard() {
+
+async function getUser() {
+    // to get all the boards created by a particular user
+    const session = await auth(); // getting the session
+    await connectMongo();
+
+    return await User.findById(session.user.id).populate("boards"); // if we have the user, it goes to this
+
+    // from the "ref" as boards in the models , as described in the populate
+}
+
+export default async function  Dashboard() {
+
+    const user = await getUser(); // gives the boards of the users as they are sessioned
+
+    console.log(user);
+
     return (
         <main className="bg-base-200 min-h-screen">
             {/* Header */}
