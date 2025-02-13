7e14deafe48c81f0ada5a3ce3c507fbb9ab20d3e
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-02-13 17:27:00 +0530

    searching board by objectId of it

--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -1,10 +1,31 @@
-export default async function FeedbackBoard({ params} ) {
-const { boardId } = params;
-
 // Get board with ObjectID 'boardId'
 
+// passing boardId to retrieve the board from the db
+// const board - finding board from the board collection
+// return board 
 
-    return <main>{boardId}</main>;
+
+// const board = gets the boardId
+
+import connectMongo from "@/libs/mongoose";
+import Board from "@/models/Board";
+
+const getBoard = async (boardId) => {
+    await connectMongo();
+
+    const board = await Board.findById(boardId);
+
+    return board;
+};
+
+export default async function FeedbackBoard({params}) {
+const { boardId } = params;
+
+const board = await getBoard(boardId);
+
+return <main>{board.name}</main>;
+
 }
+
 
 
