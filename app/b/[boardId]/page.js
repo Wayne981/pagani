99f02990a4e763863c45fa3e8a5d5062a59c0599
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-02-14 12:39:20 +0530

    Public board page

---
+++ app/b/[boardId]/page.js
@@ -0,0 +1,32 @@
+// public board
+
+import { redirect } from "next/navigation";
+import connectMongo from "@/libs/mongoose";
+import Board from "@/models/Board";
+
+const getBoard = async (boardId) => {
+
+
+    await connectMongo();
+
+    const board = await Board.findById(boardId);
+
+    if (!board) {
+        redirect("");
+    }
+    
+
+    return board;
+};
+
+export default async function PublicFeedbackBoard({params}) {
+const { boardId } = params;
+
+const board = await getBoard(boardId);
+
+return <main>{board.name} (public)</main>; // to know we are looking at a public page
+
+}
+
+
+
