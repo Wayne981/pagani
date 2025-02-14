e8eb3cf0267e793c9f84b44df39b76f153d8ea41
Chiranthan M <chiranthanmahesh007@gmail.com>
2025-02-14 12:27:17 +0530

    Board of the requested user only

--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -7,13 +7,27 @@
 
 // const board = gets the boardId
 
+import { redirect } from "next/navigation";
 import connectMongo from "@/libs/mongoose";
 import Board from "@/models/Board";
+import { auth } from "@/auth";
+
+
 
 const getBoard = async (boardId) => {
+const session = await auth();
+
     await connectMongo();
 
-    const board = await Board.findById(boardId);
+    const board = await Board.findOne({
+        _id: boardId,
+        userId : session?.user?.id, // board defined by that session's userId
+    });
+
+    if (!board) {
+        redirect("/dashboard");
+    }
+    
 
     return board;
 };
